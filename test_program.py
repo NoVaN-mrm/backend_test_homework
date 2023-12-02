fe9803443d2cf49b66310d854e59ac28b888dd37
Alexander Myalkovsky (home) <novanmab@rambler.ru>
2023-12-02 20:45:21 +0300

    добавил !

--- test_program.py
+++ test_program.py
@@ -5,7 +5,7 @@ dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
 files_list = ['program.py', 'readme.md']
 
-''' Проверить код позже'''
+
 def test_program():
     for filename in files_list:
         assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
